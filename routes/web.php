0376e100c2a1fb8e0861f7b1b6b748362d9c7b15
steeven-js <jacques.steeven@gmail.com>
2024-05-05 11:55:05 -0400

    UserResource

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return ['Laravel' => app()->version()];
+// });
+
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return redirect('/admin');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
